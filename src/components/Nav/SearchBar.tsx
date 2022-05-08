9c56a9ac19a307c455e10e6f71e5011732294db8
seulhee <3021062@gmail.com>
2022-05-09 01:11:44 +0900

    [#7] apply Nav, modify SearchBar size

--- src/components/Nav/SearchBar.tsx
+++ src/components/Nav/SearchBar.tsx
@@ -1,17 +1,28 @@
+import { css, Theme } from "@emotion/react";
 import { Input, Spacer } from "@nextui-org/react";
 import { useState } from "react";
 
 function SearchBar() {
   const [searchField, setSearchField] = useState("");
 
-  return <>
-    <Spacer y={0.5} />
-    <Input
-      size="sm"
-      placeholder="검색어를 입력해주세요."
-    />
-  </>
+  return (
+    <div css={wrapper}>
+      <Spacer y={0.5} />
+      <Input width="90%" placeholder="검색어를 입력해주세요." />
+    </div>
+  );
 }
 
-
 export default SearchBar;
+
+const wrapper = (theme: Theme) => css`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  input::placeholder {
+    font-weight: ${theme.fontWeight.light};
+    font-size: 0.74rem;
+    padding: 0 4px;
+  }
+`;
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,21 @@
 import { css, Theme } from "@emotion/react";
+import Category from "components/common/Category/TopLink";
 import SearchBar from "components/Nav/SearchBar";
+import Link from "next/link";
 
 function Nav() {
   return (
-    <div css={wrapper}>
-      <h1 css={logo}>온수냠냠냠</h1>
-      <SearchBar />
-    </div>
+    <>
+      <div css={wrapper}>
+        <Link href="/">
+          <a>
+            <h1 css={logo}>온수냠냠냠</h1>
+          </a>
+        </Link>
+        <SearchBar />
+      </div>
+      <Category />
+    </>
   );
 }
 
@@ -17,6 +26,7 @@ const wrapper = (theme: Theme) => css`
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 100%;
 
   padding: 0 22px;
   height: 60px;
@@ -25,6 +35,6 @@ const wrapper = (theme: Theme) => css`
 const logo = (theme: Theme) => css`
   font-weight: ${theme.fontWeight.bold};
   color: ${theme.color.yellow};
-  font-size: 22px;
+  font-size: 1.375rem;
   margin: 0;
 `;
