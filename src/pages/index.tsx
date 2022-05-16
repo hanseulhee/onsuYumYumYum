b2efb42c9c26af18bc99a7e3e5d6ea43b2c47d33
seulhee <3021062@gmail.com>
2022-05-16 13:54:47 +0900

    [#19] apply Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { css, Theme } from "@emotion/react";
 import Carousel from "components/common/Main/Carousel";
+import Footer from "components/Footer";
 
 function Home() {
   return (
     <>
       <Carousel />
+      <Footer />
     </>
   );
 }
