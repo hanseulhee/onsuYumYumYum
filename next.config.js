cda9bbb13a19a0258e4813915f230e2b88fc938e
hanseulhee <3021062@gmail.com>
2022-06-16 17:26:35 +0900

    [#17] install turning markdown into HTML modules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = [
+  nextConfig,
+  {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+      return config
+    },
+  },
+]
