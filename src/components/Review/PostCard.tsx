63abfc7c4bd185f8a20654193f44a3bde563f107
hanseulhee <3021062@gmail.com>
2022-07-17 22:58:11 +0900

    [#17] modify PostCard component

--- src/components/Review/PostCard.tsx
+++ src/components/Review/PostCard.tsx
@@ -1,20 +1,18 @@
 import { css, Theme } from "@emotion/react";
 import { Link } from "@nextui-org/react";
-import { PostMarkdown } from "util/getMarkdownBySlug";
 
-export interface PostListItemProps extends PostMarkdown {}
-
-function PostCard(props) {
-  const post = props;
+function PostCard(post) {
   return (
     <article css={cardWrapper}>
       <div css={contentWrapper}>
-        <Link href={`/Review/${post?.slug}`}>
+        <Link href={`/Review/${post.slug}`}>
           <a css={titleContent}>{post.title}</a>
         </Link>
         <p css={dateContent}>{post.date}</p>
         {post.tags?.map((tag, index) => (
-          <span key={index} css={tagContent}>{tag}</span>
+          <span key={index} css={tagContent}>
+            {tag}
+          </span>
         ))}
       </div>
     </article>
@@ -49,6 +47,6 @@ const dateContent = (theme: Theme) => css`
 `;
 
 const tagContent = (theme: Theme) => css`
-color: ${theme.color.black};
-font-size: 0.86rem;
+  color: ${theme.color.black};
+  font-size: 0.86rem;
 `;
