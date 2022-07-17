0e557b031303a937e74bf9f847f7ec8e66a2e5e0
hanseulhee <3021062@gmail.com>
2022-07-17 22:54:49 +0900

    [#17] dev dependencies, delete webpack

--- next.config.js
+++ next.config.js
@@ -3,15 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = [
-  nextConfig,
-  {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: 'raw-loader',
-      })
-      return config
-    },
-  },
-]
+module.exports = nextConfig
