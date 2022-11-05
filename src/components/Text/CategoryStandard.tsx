567b3481dd3aee6f8e853f98504101ffc07fdeef
hanseulhee <3021062@gmail.com>
2022-11-06 05:10:23 +0900

    [#83] apply CategoryStandard text components

---
+++ src/components/Text/CategoryStandard.tsx
@@ -0,0 +1,46 @@
+import { css, Theme } from "@emotion/react";
+import Link from "next/link";
+import ArrowRightRoundedIcon from "@mui/icons-material/ArrowRightRounded";
+
+interface Props {
+  category: string;
+}
+
+function CategoryStandard({ category }: Props) {
+  return (
+    <Link href="/Category/Standard" passHref>
+      <div css={standardWrapper}>
+        <span css={standardIcon}>
+          <ArrowRightRoundedIcon />
+        </span>
+        <button css={standardSummary}>{category} 식당의 기준</button>
+      </div>
+    </Link>
+  );
+}
+
+export default CategoryStandard;
+
+const standardWrapper = (theme: Theme) => css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: fit-content;
+  font-size: 0.75rem;
+  font-weight: ${theme.fontWeight.normal};
+  cursor: pointer;
+  :hover {
+    font-weight: ${theme.fontWeight.bold};
+    transition: all 0.3s;
+  }
+`;
+
+const standardSummary =  css`
+  margin-top: 0.223rem;
+`
+
+const standardIcon = (theme: Theme) => css`
+  display: flex;
+  align-items: center;  
+  color: ${theme.color.yellow};
+`;
