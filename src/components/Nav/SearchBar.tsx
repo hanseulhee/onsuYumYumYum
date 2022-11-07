502b78a6b13852d0c386f958653d1bc0372a3849
hanseulhee <3021062@gmail.com>
2022-11-07 21:06:06 +0900

    update Global style

--- src/components/Nav/SearchBar.tsx
+++ src/components/Nav/SearchBar.tsx
@@ -1,20 +1,20 @@
 import { css, Theme } from "@emotion/react";
-import { Input, Spacer } from "@nextui-org/react";
 import Link from "next/link";
 
 function SearchBar({ searchField, setSearchField }) {
   return (
     <Link href="/Search" passHref>
       <div css={wrapper}>
-        <Spacer y={0.5} />
-        <Input
-          width="90%"
-          placeholder="검색어를 입력해주세요."
-          value={searchField}
-          onChange={(e) => {
-            setSearchField(e.target.value);
-          }}
-        />
+        <div css={inputWrapper}>
+          <input
+            css={inputCss}
+            placeholder="검색어를 입력해주세요."
+            value={searchField}
+            onChange={(e) => {
+              setSearchField(e.target.value);
+            }}
+          />
+        </div>
       </div>
     </Link>
   );
@@ -24,12 +24,29 @@ export default SearchBar;
 
 const wrapper = (theme: Theme) => css`
   display: flex;
-  justify-content: center;
   width: 100%;
-
+  margin-left: 1.76rem;
   input::placeholder {
     font-weight: ${theme.fontWeight.light};
     font-size: 0.74rem;
-    padding: 0 0.25rem;
   }
+`;
+
+const inputWrapper = css`
+  flex: 1;
+  position: relative;
+  align-items: center;
+`;
+
+const inputCss = (theme: Theme) => css`
+  outline: none;
+  border: none;
+  background-color: #f4f4f4;
+  border-radius: 12px;
+  width: 100%;
+  height: 100%;
+  padding: 0.77rem 0.815rem;
+
+  font-weight: ${theme.fontWeight.normal};
+  font-size: 0.87rem;
 `;
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -14,12 +14,16 @@ const globalStyle = (theme: Theme) => css`
     font-family: "Noto Sans KR", sans-serif;
     font-display: swap;
     word-break: keep-all;
+    text-rendering: geometricPrecision;
+    -webkit-tap-highlight-color: transparent;
   }
 
   html,
   body {
     background-color: ${theme.color.fullWhite};
     color: ${theme.color.black};
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
   }
 
   ul,
@@ -39,5 +43,41 @@ const globalStyle = (theme: Theme) => css`
     width: 100%;
     border: none;
     background-color: #d5d8dd;
+  }
+
+  p {
+    margin: 1rem 0;
+    letter-spacing: -0.05em;
+    line-height: 1.625;
+    font-weight: 400;
+  }
+
+  summary {
+    cursor: pointer;
+    -webkit-user-select: none;
+    user-select: none;
+    list-style: none;
+    outline: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: inherit;
+    margin: 0 0 0.625rem 0;
+    letter-spacing: -0.05em;
+    line-height: 1.5;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  h4 {
+    font-size: 1.25rem;
   }
 `;
