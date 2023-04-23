1bdd2da4d493c4216bf4e3d22530beaa3c89cfe4
hanseulhee <3021062@gmail.com>
2023-04-24 02:08:44 +0900

    [#90] apply Search page

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,29 +1,44 @@
 import { css, Theme } from "@emotion/react";
+import SearchResult from "components/common/SearchResult";
+import Nav from "components/Nav";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
 
 function Search() {
+  const [searchField, setSearchField] = useState<string>("");
+
+  function onChange(e: ChangeEvent<HTMLInputElement>) {
+    setSearchField(e.target.value);
+  }
+
+  // function onClick() {
+  //   setSearchField("");
+  // }
+
   return (
-    <div css={wrapper}>
-      <div css={textWrapper}>
-        {/* <span css={text}>{searchField} 검색 결과</span>
-        <Image
-          src="/favicon/logo.png"
-          alt="logo img"
-          width="20%"
-          height="20%"
-        /> */}
+    <>
+      <Nav search={searchField} onChange={onChange} />
+      <div css={wrapper}>
+        <div css={textWrapper}>
+          <span css={text}>{searchField} 검색 결과</span>
+          <Image
+            src="/favicon/logo.png"
+            alt="logo img"
+            width="20%"
+            height="20%"
+          />
+        </div>
+        <SearchResult searchField={searchField} />
       </div>
-      <div css={cardWrapper}>
-        <span css={errorText}>헉! 공사 중이에요 🔧🏃🏻</span>
-      </div>
-    </div>
+    </>
   );
 }
 
 export default Search;
 
 const wrapper = css`
-  min-height: 100%;
-  position: relative;
+  display: flex;
+  flex-direction: column;
   width: 100%;
 `;
 
@@ -31,17 +46,7 @@ const textWrapper = css`
   display: flex;
   flex-direction: row;
   align-items: center;
-  width: 100%;
-  padding: 0.8rem 1.25rem;
-`;
-
-const cardWrapper = css`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 100%;
-  min-height: 100vh;
-  flex-wrap: wrap;
+  padding: 0.8rem 0rem;
 `;
 
 const text = (theme: Theme) => css`
@@ -49,11 +54,4 @@ const text = (theme: Theme) => css`
   font-size: 1rem;
   margin-right: 0.1rem;
   padding-bottom: 0.16rem;
-`;
-
-const errorText = (theme: Theme) => css`
-  font-size: 2.6rem;
-  font-weight: ${theme.fontWeight.bold};
-  color: ${theme.color.black};
-  letter-spacing: 0.02px;
 `;
