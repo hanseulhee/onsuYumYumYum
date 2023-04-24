08b10bf18c40f1cea889af998d98dfc9de408c8f
hanseulhee <3021062@gmail.com>
2023-04-25 00:02:19 +0900

    [#103] add cafe topCategory

--- src/components/common/Category/TopLink.tsx
+++ src/components/common/Category/TopLink.tsx
@@ -4,12 +4,15 @@ import TopCategory from "components/common/Category/TopCategory";
 function TopLink() {
   return (
     <header css={wrapper}>
-      <TopCategory path="/" category="홈" />
-      <TopCategory path="/Category/Alone" category="혼밥" />
-      <TopCategory path="/Category/Dining" category="회식" />
-      <TopCategory path="/Category/Hangover" category="해장" />
-      <TopCategory path="/Category/CostEffect" category="가성비" />
-      <TopCategory path="/Category/Ricefull" category="밥full" />
+      <div css={inWrapper}>
+        <TopCategory path="/" category="홈" />
+        <TopCategory path="/Category/Alone" category="혼밥" />
+        <TopCategory path="/Category/Dining" category="회식" />
+        <TopCategory path="/Category/Hangover" category="해장" />
+        <TopCategory path="/Category/CostEffect" category="가성비" />
+        <TopCategory path="/Category/Ricefull" category="밥full" />
+        <TopCategory path="/Category/Cafe" category="카페" />{" "}
+      </div>
     </header>
   );
 }
@@ -17,11 +20,21 @@ function TopLink() {
 export default TopLink;
 
 const wrapper = css`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
   width: 100%;
   padding: 0 5px;
   height: auto;
   padding-bottom: 0.5rem;
+`;
+
+const inWrapper = css`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  overflow-x: scroll;
+  overflow-y: hidden;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  cursor: pointer;
 `;
