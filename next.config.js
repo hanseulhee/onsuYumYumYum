218c96a7ea3daaffbd718b090a27e22c7fc59551
hanseulhee <3021062@gmail.com>
2023-07-25 16:19:26 +0900

    add github img

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ module.exports = {
       'mblogthumb-phinf.pstatic.net',
       '52.79.254.207',
       'api.onsuyum.com',
+      'github.com',
       process.env.NEXT_PUBLIC_IMAGE_URL,
     ],
     formats: ['image/avif', 'image/webp'],
