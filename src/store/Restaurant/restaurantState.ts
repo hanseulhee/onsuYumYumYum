9dcadf985bc1f27f7286552b5eaff330d3ec1735
hanseulhee <3021062@gmail.com>
2024-04-01 14:51:45 +0900

    [#126] apply useIntersectionObserver hook

--- src/store/Restaurant/restaurantState.ts
+++ src/store/Restaurant/restaurantState.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
+import { v1 } from "uuid";
 
 export const restaurantState = atom<IGetRestaurantDataContent[]>({
-  key: "restaurantState",
+  key: `restaurantState/${v1()}`,
   default: [],
 });
