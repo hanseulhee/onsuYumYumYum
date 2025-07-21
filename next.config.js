cfc3be8cc7570fc75c33f797912f8fcc31baaf6f
hanseulhee <3021062@gmail.com>
2025-07-22 08:42:18 +0900

    apply unoptimized

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  output: "export",
   reactStrictMode: true,
   images: {
     domains: [
-      'user-images.githubusercontent.com',
-      'localhost',
-      'search.pstatic.net',
-      'mblogthumb-phinf.pstatic.net',
-      '52.79.254.207',
-      'api.onsuyum.com',
+      "user-images.githubusercontent.com",
+      "localhost",
+      "search.pstatic.net",
+      "mblogthumb-phinf.pstatic.net",
+      "52.79.254.207",
+      "api.onsuyum.com",
       // process.env.NEXT_PUBLIC_IMAGE_URL,
     ],
-    formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
   },
-}
+};
